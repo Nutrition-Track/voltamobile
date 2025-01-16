16c0b484f5df9e7e2f18168fddfa78e97cc1e353
david stall <davidstall312@gmail.com>
2025-01-16 07:40:30 -0800

    added header

--- lib/main.dart
+++ lib/main.dart
@@ -6,26 +6,34 @@ class SelectionContainerDisabledExampleApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var aspectratio = 16/9;
-    var uielementpadding = 11.5;
+    const aspectratio = 16/9; // WARNING! CHANGING THIS VALUE MAY LEAD TO UNEXPECTED CONSEQUENCES WITH SCALING!!!
+    const uielementpadding = 11.5; // WARNING! CHANGING THIS VALUE MAY LEAD TO UNEXPECTED CONSEQUENCES WITH SCALING!!!
+    const screenwidth = 400.00; // WARNING! CHANGING THIS VALUE MAY LEAD TO UNEXPECTED CONSEQUENCES WITH SCALING!!!
     return Center(
       child: Container(
         color: Colors.blue[50],
         child: Center(
           child: Container(
-            width: 400,
-            height: 400 * aspectratio,
+            width: screenwidth,
+            height: screenwidth * aspectratio,
             alignment: Alignment.topLeft,
             color: Colors.blue[400],
             child: Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.all(uielementpadding),
-                child: Container(
-                  width: 380,
-                  height: (400 * aspectratio) / 6,
-                  color: Colors.blue[300],
-                )
+            children: [ // list of boxes
+              Row( // header row
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    width: screenwidth / 3,
+                    height: 30,
+                    color: Colors.red[200]
+                  ),
+                  Container(
+                    width: screenwidth / 6,
+                    height: 30,
+                    color: Colors.red[400]
+                  )
+                ]
               ),
               Padding(
                 padding: EdgeInsets.all(uielementpadding),
