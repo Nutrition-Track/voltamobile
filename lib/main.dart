4268050d63e7df3377308a59297ccfc471600426
david stall <davidstall312@gmail.com>
2025-01-20 15:27:30 -0800

    added a few changes to match figma

--- lib/main.dart
+++ lib/main.dart
@@ -2,141 +2,126 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(const SelectionContainerDisabledExampleApp());
 
+// pink color: (215, 98, 244) (R, G, B)
 class SelectionContainerDisabledExampleApp extends StatelessWidget {
   const SelectionContainerDisabledExampleApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    DateTime now = new DateTime.now();
+    DateTime date = new DateTime(now.year, now.month, now.day);
+    String day;
+    if (date.weekday == 0) {
+      day = "Sunday";
+    } else if (date.weekday == 1) {
+      day = "Monday";
+    } else if (date.weekday == 2) {
+      day = "Tuesday";
+    } else if (date.weekday == 3) {
+      day = "Wednesday";
+    } else if (date.weekday == 4) {
+      day = "Thursday";
+    } else if (date.weekday == 5) {
+      day = "Friday";
+    } else {
+      day = "Saturday";
+    }
     const aspectratio = 16 /
         9; // WARNING! CHANGING THIS VALUE MAY LEAD TO UNEXPECTED CONSEQUENCES WITH SCALING!!!
     const uielementpadding =
         11.5; // WARNING! CHANGING THIS VALUE MAY LEAD TO UNEXPECTED CONSEQUENCES WITH SCALING!!!
     const screenwidth =
         400.00; // WARNING! CHANGING THIS VALUE MAY LEAD TO UNEXPECTED CONSEQUENCES WITH SCALING!!!
+    const screenheight = screenwidth * aspectratio;
     return Container(
         color: Colors.blue[50],
-        child: Center(
-            child: Container(
-                width: screenwidth,
-                height: screenwidth * aspectratio,
-                alignment: Alignment.topLeft,
-                decoration: BoxDecoration(
-                  color: Colors.blue[400],
-                  borderRadius: BorderRadius.circular(15.0),
-                ),
-                child: Column(children: [
-                  // list of boxes
-                  Padding(
-                      padding: EdgeInsets.all(uielementpadding),
-                      child: Container(
-                          decoration: BoxDecoration(
-                            color: Colors.red[400],
-                            borderRadius: BorderRadius.circular(15.0),
-                          ),
+        child: Directionality(
+            textDirection: TextDirection.ltr,
+            child: Scaffold(
+                body: Container(
+                    width: screenwidth,
+                    height: screenheight,
+                    alignment: Alignment.topLeft,
+                    decoration: BoxDecoration(
+                      color: Colors.blue[400],
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    child: Column(children: [
+                      // list of boxes
+                      Padding(
+                          // header
+                          padding: EdgeInsets.all(uielementpadding),
+                          child: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.red[400],
+                                borderRadius: BorderRadius.circular(15.0),
+                              ),
+                              width: screenwidth,
+                              height: 30,
+                              child: Row(
+                                  // header row
+                                  textDirection: TextDirection
+                                      .ltr, // this is required for the row to render properly
+                                  mainAxisSize: MainAxisSize.max,
+                                  children: <Widget>[
+                                    Padding(
+                                        padding: EdgeInsets.all(6),
+                                        child: Container(
+                                          // left widget, user menu
+                                          width: screenwidth / 3,
+                                          height: 30,
+                                          decoration: BoxDecoration(
+                                            color: Colors.red[200],
+                                            borderRadius:
+                                                BorderRadius.circular(15.0),
+                                          ),
+                                        )),
+                                    Padding(
+                                      padding: EdgeInsets.all(6),
+                                      child: Container(
+                                          // right widget, welcome text
+                                          width: screenwidth / 6,
+                                          height: 30,
+                                          decoration: BoxDecoration(
+                                            color: Colors.red[200],
+                                            borderRadius:
+                                                BorderRadius.circular(15.0),
+                                          )),
+                                    ),
+                                  ]))),
+                      Padding(
+                          // welcome text
+                          padding: EdgeInsets.all(uielementpadding),
+                          child: DefaultTextStyle.merge(
+                              style: TextStyle(
+                                  fontSize: 24, fontWeight: FontWeight.bold),
+                              child: Center(
+                                  child: Text(
+                                      'Welcome\n' +
+                                          date.month.toString() +
+                                          '/' +
+                                          date.day.toString() +
+                                          '/' +
+                                          date.year.toString() +
+                                          ' - ' +
+                                          day,
+                                      textAlign: TextAlign.center)))),
+                      Padding(
+                        // progress bar
+                          padding: EdgeInsets.all(5),
+                          child: LinearProgressIndicator(
+                            value: 0.66,
+                            minHeight: 30,
+                            color: Color.fromARGB(255, 215, 98, 244),
+                            backgroundColor: Color.fromARGB(255, 255, 255, 255),
+                          )),
+                      Padding(
+                        padding: EdgeInsets.all(5),
+                        child: SizedBox(
                           width: screenwidth,
-                          height: 30,
-                          child: Row(
-                              // header row
-                              textDirection: TextDirection
-                                  .ltr, // this is required for the row to render properly
-                              mainAxisSize: MainAxisSize.max,
-                              children: <Widget>[
-                                Padding(
-                                    padding: EdgeInsets.all(6),
-                                    child: Container(
-                                      // left widget, user menu
-                                      width: screenwidth / 3,
-                                      height: 30,
-                                      decoration: BoxDecoration(
-                                        color: Colors.red[200],
-                                        borderRadius:
-                                            BorderRadius.circular(15.0),
-                                      ),
-                                    )),
-                                Padding(
-                                  padding: EdgeInsets.all(6),
-                                  child: Container(
-                                      // right widget, welcome text
-                                      width: screenwidth / 6,
-                                      height: 30,
-                                      decoration: BoxDecoration(
-                                        color: Colors.red[200],
-                                        borderRadius:
-                                            BorderRadius.circular(15.0),
-                                      )),
-                                ),
-                              ]))),
-                  Padding(
-                    padding: EdgeInsets.all(uielementpadding),
-                    child: Container(
-                      // diet progress
-                      width: 380,
-                      height: (400 * aspectratio) / 6,
-                      decoration: BoxDecoration(
-                        color: Colors.blue[300],
-                        borderRadius: BorderRadius.circular(15.0),
+                          child: Text('___/2000 calories', textAlign: TextAlign.left),
+                        )
                       ),
-                    ),
-                  ),
-                  Padding(
-                      padding: EdgeInsets.all(uielementpadding),
-                      child: Container(
-                          // food inputs
-                          width: 380,
-                          height: (400 * aspectratio) / 6,
-                          decoration: BoxDecoration(
-                            color: Colors.blue[300],
-                            borderRadius: BorderRadius.circular(15.0),
-                          ),
-                          child: Row(
-                              textDirection: TextDirection.ltr, // left to right
-                              children: [
-                                Padding(
-                                    // food inputs barcode
-                                    padding: EdgeInsets.all(6),
-                                    child: Container(
-                                      decoration: BoxDecoration(
-                                        color: Colors.green[300],
-                                        borderRadius:
-                                            BorderRadius.circular(15.0),
-                                      ),
-                                      width: screenwidth / 4,
-                                    )),
-                                Padding(
-                                    // food inputs ai (center!)
-                                    padding: EdgeInsets.all(6),
-                                    child: Container(
-                                      decoration: BoxDecoration(
-                                        color: Colors.green[300],
-                                        borderRadius:
-                                            BorderRadius.circular(15.0),
-                                      ),
-                                      width: screenwidth / 4,
-                                    )),
-                                Padding(
-                                    // food inputs manual
-                                    padding: EdgeInsets.all(6),
-                                    child: Container(
-                                      decoration: BoxDecoration(
-                                        color: Colors.green[300],
-                                        borderRadius:
-                                            BorderRadius.circular(15.0),
-                                      ),
-                                      width: screenwidth / 4,
-                                    )),
-                              ]))),
-                  Padding(
-                    // calendar
-                    padding: EdgeInsets.all(uielementpadding),
-                    child: Container(
-                      width: 380,
-                      height: (400 * aspectratio) / 6,
-                      decoration: BoxDecoration(
-                        color: Colors.blue[300],
-                        borderRadius: BorderRadius.circular(15.0),
-                      ),
-                    ),
-                  ),
-                ]))));
+                    ])))));
   }
 }
